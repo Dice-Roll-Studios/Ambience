75a730fcb0b0a9c561d642485ca7d2183c8b2ba7
CDX <80155611+CDX-1@users.noreply.github.com>
2024-03-17 07:59:06 -0400

    Create restrictions.lua

---
+++ src/restrictions.lua
@@ -0,0 +1,96 @@
+return {
+	
+	["Lighting"] = {
+
+		["Ambient"] = "Color3",
+		["Brightness"] = "number",
+		["ColorShift_Bottom"] = "Color3",
+		["ColorShift_Top"] = "Color3",
+		["EnvironmentDiffuseScale"] = "number",
+		["EnvironmentSpecularScale"] = "number",
+		["GlobalShadows"] = "boolean",
+		["OutdoorAmbient"] = "Color3",
+		["ShadowSoftness"] = "number",
+
+		["ClockTime"] = "number",
+		["GeographicLatitude"] = "number",
+		["TimeOfDay"] = "string",
+
+		["ExposureCompensation"] = "number"
+
+	},
+	
+	["Atmosphere"] = {
+		
+		["Density"] = "number",
+		["Offset"] = "number",
+		
+		["Color"] = "Color3",
+		["Decay"] = "Color3",
+		["Glare"] = "number",
+		["Haze"] = "number"
+		
+	},
+	
+	["Sky"] = {
+		
+		["CelestialBodiesShown"] = "boolean",
+		["MoonAngularSize"] = "number",
+		["MoonTextureId"] = "string",
+		["SkyboxBk"] = "string",
+		["SkyboxDn"] = "string",
+		["SkyboxFt"] = "string",
+		["SkyboxLf"] = "string",
+		["SkyboxRt"] = "string",
+		["SkyboxUp"] = "string",
+		["StarCount"] = "number",
+		["SunAngularSize"] = "number",
+		["SunTextureId"] = "string"
+		
+	},
+	
+	["DepthOfFieldEffect"] = {
+		
+		["Enabled"] = "boolean",
+		["FarIntensity"] = "number",
+		["FocusDistance"] = "number",
+		["InFocusRadius"] = "number",
+		["NearIntensity"] = "number"
+		
+	},
+	
+	["BloomEffect"] = {
+
+		["Enabled"] = "boolean",
+		["Intensity"] = "number",
+		["Size"] = "number",
+		["Threshold"] = "number"
+
+	},
+	
+	["SunRaysEffect"] = {
+		
+		["Enabled"] = "boolean",
+		["Intensity"] = "number",
+		["Spread"] = "number"
+		
+	},
+	
+	["ColorCorrectionEffect"] = {
+		
+		["Brightness"] = "number",
+		["Contrast"] = "number",
+		["Enabled"] = "boolean",
+		["Saturation"] = "number",
+		["TintColor"] = "Color3"
+		
+	},
+	
+	["BlurEffect"] = {
+		
+		["Enabled"] = "number",
+		["Size"] = "number"
+		
+	}
+	
+}
